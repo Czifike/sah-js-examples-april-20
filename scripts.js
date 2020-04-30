4da4f7d581dbfb55572470a8958161fdb014e055
Czifike <64535170+Czifike@users.noreply.github.com>
2020-04-30 19:39:49 +0200

    add jquery example

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Third part:
+
+$('h1').html('This is <em> done by </em> JavaScript');
+$('p').css('border', '1px solid red');
+$('p').css('padding', '10px');
+$('h2').toggleClass('blue');
+
+
+console.log('Script file is loaded.')
+
+
+// Single line comments
+//jQuery helpd us

920695c01d6c45b3236d5cb280d1db9e8ecb3ad1
Czifike <64535170+Czifike@users.noreply.github.com>
2020-04-30 20:11:27 +0200

    add click function

--- scripts.js
+++ scripts.js
@@ -1,12 +1,29 @@
 // Third part:
+console.log('Script file is loaded.')
 
 $('h1').html('This is <em> done by </em> JavaScript');
-$('p').css('border', '1px solid red');
-$('p').css('padding', '10px');
+
 $('h2').toggleClass('blue');
 
+let name = 'Niki';
+let age = 29;
 
-console.log('Script file is loaded.')
+function mylittleFunction() {
+    $('main').append('<p>A p is appendid in the main.</p>')
+    $('p').css('border', '1px solid red');
+    $('p').css('padding', '10px');
+}
+
+mylittleFunction();
+mylittleFunction();
+
+$('#add').click(mylittleFunction);
+
+$('#remove').click(function () {
+    $('p:last-child').remove();
+});
+
+
 
 
 // Single line comments
